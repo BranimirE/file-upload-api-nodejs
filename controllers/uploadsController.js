2ea61f88585b0e350f5347b8026febfe5672af4b
Branimir Espinoza <brani.espinoza@gmail.com>
2023-09-25 18:06:35 -0400

    Finishing upload endpoint

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,23 @@
 const path = require('path')
 const Product = require('../models/Product')
 const {StatusCodes} = require('http-status-codes')
+const CustomError = require('../errors')
+const { MAX_SIZE_FILE } = process.env
 
 const uploadProductImage  = async (req, res) => {
+  // check if the file exists
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No File Uploaded')
+  }
+  // check the file type
+  if (!req.files.image.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Upload only images')
+  }
+  // check the size
+  if (req.files.image.size > MAX_SIZE_FILE) {
+    throw new CustomError.BadRequestError(`Upload images smaller than ${MAX_SIZE_FILE} bytes.`)
+  }
+
   const productImage = req.files.image;
   const imagePath = path.join(__dirname, '../public/uploads/' + productImage.name)
   await productImage.mv(imagePath)
